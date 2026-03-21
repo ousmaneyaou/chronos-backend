b00078f631ba362144669f7c8f6c12478c7416fa
ousmane <ousmaneyaou00@gmail.com>
2026-03-21 20:05:27 +0000

    modifiation du dockerfile

--- src/main/java/com/GimPay/Integration_APIs/enums/PaymentMethod.java
+++ src/main/java/com/GimPay/Integration_APIs/enums/PaymentMethod.java
@@ -2,5 +2,6 @@ package com.GimPay.Integration_APIs.enums;
 
 public enum PaymentMethod {
     CARD_3DS_OFF,   // Paiement carte sans 3DS (Disable3DS = true)
-    CARD_3DS_ON     // Paiement carte avec 3DS (Disable3DS = false)
+    CARD_3DS_ON,     // Paiement carte avec 3DS (Disable3DS = false)
+    PAYLINK
 }
